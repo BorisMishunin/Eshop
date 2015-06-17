9003bcf49aaac90761577662e1e90762be12ea2b
Django Server <django_git@django-serv>
2015-06-17 19:50:57 +0300

    Add application GOODS

---
+++ goods/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url, patterns
+
+
+
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'webcams.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^test/', 'goods.views.test'),
+
+
+]
