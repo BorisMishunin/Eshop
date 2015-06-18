1d68434ecaa7560fe09063d8164d5f028601b743
Your Name <you@example.com>
2015-06-18 23:16:12 +0300

    Registration models in admin

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from goods.models import *
 
-# Register your models here.
+admin.site.register(Producers)
+admin.site.register(Categories)
+admin.site.register(Wares)
+admin.site.register(Properties)
+admin.site.register(PropertiesValues)
+admin.site.register(PropertiesByCategories)
+admin.site.register(WaresProperties)
+admin.site.register(WaresImages)
+admin.site.register(Actions)
+admin.site.register(ActionWares)
