ee88e3e1f8557f5808b52d5d2bdc4373a9f20691
Your Name <you@example.com>
2015-06-20 18:07:02 +0300

    Setup admin.py

--- goods/admin.py
+++ goods/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 from goods.models import *
 
+class WaresFotosInLine(admin.StackedInline):
+    model = WaresImages
+    extra = 5
+
+class WaresPropertiesInLine(admin.StackedInline):
+    model = WaresProperties
+    extra = 2
+
+
+class AdminWares(admin.ModelAdmin):
+    inlines = [WaresFotosInLine, WaresPropertiesInLine, ]
+
+
 admin.site.register(Producers)
 admin.site.register(Categories)
-admin.site.register(Wares)
+admin.site.register(Wares, AdminWares)
 admin.site.register(Properties)
 admin.site.register(PropertiesValues)
 admin.site.register(PropertiesByCategories)
